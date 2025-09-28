399b3487772bb9f4a2e58524c5704c8534e4b0f8
VougneFroid <142602426+VougneFroid@users.noreply.github.com>
2025-09-28 23:58:51 +0800

    template cleaned

--- hangarinsite/todohan/views.py
+++ hangarinsite/todohan/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from todohan.models import Priority
+
+
+class HomePageView(ListView):
+    model = Priority
+    context_object_name = 'home'
+    template_name = "home.html"
+
+
+
+
 
 # Create your views here.
